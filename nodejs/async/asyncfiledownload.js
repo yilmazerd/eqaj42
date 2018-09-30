0bfe10be593ec7e2255d9189c77f17c2b74e4199
yilmazerd <yilmazerdem@gmail.com>
2018-09-30 17:15:07 -0400

    Create asyncfiledownload.js
    Using promises, download a file and write to disk 
    https://medium.com/dev-bits/writing-neat-asynchronous-node-js-code-with-promises-32ed3a4fd098

---
+++ nodejs/async/asyncfiledownload.js
@@ -0,0 +1,54 @@
+var request = require("request");
+var fs = require('fs');
+const cm = require('./combineMovies');
+const AWS = require('fluent-ffmpeg');
+
+
+function initialize(inputURL) {
+    // Setting URL and headers for request
+    var options = {
+        url: inputURL,
+        encoding: 'binary',
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    // Return new promise 
+    return new Promise(function(resolve, reject) {
+    	// Do async job
+        request.get(options, function(err, resp, body) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(body);
+            }
+        })
+    })
+
+}
+
+function main(URLinput,outputFileName,ch) {
+    var mediaFile = [];
+    var initializePromise = initialize(URLinput);
+    initializePromise.then(function(result) {
+        userDetails = result;
+        console.log("Initialized user details");
+        // Use user details from here
+        return userDetails;
+    }, function(err) {
+        console.log(err);
+    }).then(function(result) {
+    	//var buf = new Buffer.alloc(result, 'base64');
+        fs.writeFile(outputFileName, result, 'binary', function(err) {if(err) {return console.log(err);}console.log("The file was saved!");
+    }).then(function(){
+    	if (ch==2) {
+    		console.log("got this");
+    	}
+    }) 
+    });
+    //fs.writeFile('test.mp4', mediaFile, function(err) {if(err) {return console.log(err);}console.log("The file was saved!");});
+}
+
+
+main('https://github.com/mediaelement/mediaelement-files/raw/master/big_buck_bunny.mp4','test2.mp4',1);
+main('https://github.com/mediaelement/mediaelement-files/raw/master/big_buck_bunny.mp4','test21.mp4',2);
