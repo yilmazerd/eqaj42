af599367f335b3bd6b9f38472a5d7dad71f3f95c
yilmazerd <yilmazerdem@gmail.com>
2018-10-25 23:41:37 -0400

    Create howToKeyAfunction.js

---
+++ elequentjs/howToKeyAfunction.js
@@ -0,0 +1,9 @@
+//Javascript how to key a function
+
+function f2(r) {return r*2};
+function f3(r) {return r*3};
+function f4(r) {return r*4};
+
+_myFunAr = {key1:f2, key2:f3, key3:f4};
+
+_myFunAr['key1'](11.2)
